6594eb5292f024b9869ef24d8c90112d8e698224
Maksym Bilenko <sath891@gmail.com>
2014-11-18 17:24:29 +0200

    Add domian recursions support

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,9 @@
     echo gmysql-password=$MYSQL_DBPASS >> /etc/powerdns/pdns.d/pdns.local.gmysql.conf 
     sed -i s/..recursor=/recursor=$RECURSOR1\ $RECURSOR2/g /etc/powerdns/pdns.conf 
     sed -i s/allow-recursion=127.0.0.1/allow-recursion=$ALLOW_RECURSION_MASK/g /etc/powerdns/pdns.conf 
+if [ -f /mnt/recursors.conf ]; then
+    echo forward-zones-file=/mnt/recursors.conf >> /etc/powerdns/pdns.conf
+fi
     cat /etc/powerdns/pdns.d/pdns.local.gmysql.conf 
     rm -f /etc/powerdns/pdns.d/pdns.simplebind.conf 
     exec "$@"
